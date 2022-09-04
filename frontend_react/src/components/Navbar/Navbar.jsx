9d0df0330c8d25233e268005cc6f57e3d781a51d
Salila Herath <salilaherath@gmail.com>
2022-09-04 16:32:56 +0530

    Navbar desktop version created

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 
+import { images } from '../../constants';
 import './Navbar.scss';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className='app__navbar'>
+      <div className='app__navbar__logo'>
+        <img src={images.logo} alt='' />
+      </div>
+      <ul className='app__navbar-links'>
+        {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+          <li className='app__flex p-text' key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
